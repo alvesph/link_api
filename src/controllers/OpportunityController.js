aeea7ea30d61a94d64c7ef69557579ce1692e116
alvesph <alvesph1@gmail.com>
2021-10-03 21:31:50 -0300

    Cadastramento de nova Oportunidades

--- src/controllers/OpportunityController.js
+++ src/controllers/OpportunityController.js
@@ -1,8 +1,33 @@
-const Opportunity = require('../models/OpportunityModel');
+const OpportunityModel = require('../models/OpportunityModel');
 
-module.exports = {
-  async index(request, response) {
-    const oportunities = await Opportunity.find();
-    return response.status(200).json(oportunities)
+class OpportunityController{
+  
+  async create(req, res){
+
+    const opportunity = new OpportunityModel(req.body);
+    await opportunity 
+          .save()
+          .then(response => {
+            return res.status(200).json(response)
+          })
+          .catch(error => {
+            return res.status(500).json(error)
+          });
   }
+
+  async show(req, res){
+    await opportunity.findById(req.params.id)
+          .then(response => {
+            if(response)
+              return res.status(200).json(response);
+            else
+              return res.status(404).json({error: 'Not find'});
+          })
+          .catch(error => {
+            return res.status(500).json(error);
+          })
+  }
+
 }
+
+module.exports = new OpportunityController();
